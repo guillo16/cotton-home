d6443a627264d0545269d4d169eef946cce13854
guillermo varela <guillovarela16@gmail.com>
2020-10-27 18:20:08 -0300

    Add test user

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,11 +2,22 @@ require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
+    login_as users(:admin)
     @category = Category.new(
       name: "Hogar",
       photo: "url"
       )
   end
 
-  test ""
+  test "buyer should be able to see all categories" do
+    get categories_path
+
+    assert_response :success
+  end
+
+  test "redirected if not admin" do
+    get "/categories/new"
+
+    assert_response :redirected
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.new(
+      email: "buyer2@mail.com",
+      password: '123456',
+      permission_level: "buyer",
+    )
+  end
+
+  test "should be valid if all fields are set" do
+    assert @user.valid?
+  end
+
+  test "should be invalid if email is not present" do
+    @user.email = nil
+
+    assert_not @user.valid?
+  end
+
+  test "should be invalid if password is not present" do
+    @user.password = nil
+
+    assert_not @user.valid?
+  end
+
+  test "should be invalid if user already exists" do
+    @user.email = "buyer@mail.com"
+
+    assert_not @user.valid?
+  end
 end
