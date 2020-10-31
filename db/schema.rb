4ec695442311d74eb23e838b39e8e6a1f79a13dc
guillermo varela <guillovarela16@gmail.com>
2020-10-31 15:33:31 -0300

    Add product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_27_210010) do
+ActiveRecord::Schema.define(version: 2020_10_31_183243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2020_10_27_210010) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
     t.index ["slug"], name: "index_categories_on_slug", unique: true
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.bigint "sub_category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sub_category_id"], name: "index_products_on_sub_category_id"
   end
 
   create_table "sub_categories", force: :cascade do |t|
@@ -47,5 +56,6 @@ ActiveRecord::Schema.define(version: 2020_10_27_210010) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "sub_categories"
   add_foreign_key "sub_categories", "categories"
 end
