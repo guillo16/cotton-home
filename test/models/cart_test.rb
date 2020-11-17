5232bf7135db5f5cf663c032067fec4b4c9ee03e
guillermo varela <guillovarela16@gmail.com>
2020-11-17 17:22:43 -0300

    Finish tests

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @cart = Cart.new
+  end
+
+  test "should be valid if all fields are set" do
+    assert @cart.valid?
+  end
 end
--- test/models/line_item_test.rb
+++ test/models/line_item_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class LineItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+   @line_item = LineItem.new(
+     product_id: products(:sheets).id,
+     variant_id: variants(:large).id,
+     cart_id: carts(:first_cart).id
+    )
+  end
+
+  test "should be valid if all fields are set" do
+    assert @line_item.valid?
+  end
+
+   test "should be invalid if quantity is not present" do
+    @line_item.quantity = nil
+
+    assert_not @line_item.valid?
+  end
+
+  test "should be invalid if product is not present" do
+    @line_item.product_id = nil
+
+    assert_not @line_item.valid?
+  end
+
+  test "should be invalid if variant is not present" do
+    @line_item.variant_id = nil
+
+    assert_not @line_item.valid?
+  end
+
+  test "should be invalid if cart is not present" do
+    @line_item.cart_id = nil
+
+    assert_not @line_item.valid?
+  end
 end
