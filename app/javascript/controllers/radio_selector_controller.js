0630eb291e0c78da5142e3972bec54da28b52bec
guillermo varela <guillovarela16@gmail.com>
2020-11-20 10:49:51 -0300

    Fixing stimulus in progress

--- app/javascript/controllers/radio_selector_controller.js
+++ app/javascript/controllers/radio_selector_controller.js
@@ -5,11 +5,14 @@ export default class extends Controller {
   static targets = ["option", "input", "submit", "text", "guillo"]
 
   selectRadioOption() {
-    this.optionTargets.forEach((el, i) => {
-      el.classList.toggle("active", event.target == el )
+    event.preventDefault()
+    this.optionTargets.forEach((something, i) => {
+      something.classList.toggle("active", event.currentTarget == something)
     })
-
-    this.inputTarget.value = event.target.innerText
+    // this.optionTargets.forEach((el, i) => {
+    //   el.classList.toggle("active", event.target == el )
+    // })
+    this.inputTarget.value = event.currentTarget.innerText
   }
 }
 
