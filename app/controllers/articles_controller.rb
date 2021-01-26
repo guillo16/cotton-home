a99879658c239a8aba2a333a947b3138a571bcea
guillermo varela <guillovarela16@gmail.com>
2021-01-26 19:33:56 -0300

    In progress

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,74 @@
 class ArticlesController < ApplicationController
+  before_action :set_articles, only: %i[update destroy]
+
   def index
+    if user_is_wholesaler?
+      @articles = Article.all
+      @division = Division.all
+    else
+      flash[:notice] = "Accesso denegado!"
+      redirect_to root_path
+    end
   end
 
   def show
-  end
-
-  def edit
+    if user_is_wholesaler?
+      @article = Article.friendly.find(params[:id])
+    else
+      flash[:notice] = "Accesso denegado!"
+      redirect_to root_path
+    end
   end
 
   def new
+    if user_has_permission_level?
+      @article = Article.new
+    else
+      flash[:notice] = "Accesso denegado!"
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    if user_has_permission_level?
+      @article = Article.friendly.find(params[:id])
+    else
+      flash[:notice] = "Accesso denegado!"
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    return unless current_user.permission_level == "super_admin"
+
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_articles
+    @article = Article.friendly.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:name, :division_id, photos: [])
   end
 end
--- app/models/division.rb
+++ app/models/division.rb
@@ -1,3 +1,8 @@
 class Division < ApplicationRecord
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
+  has_many :articles
+
   validates :name, presence: true
 end
