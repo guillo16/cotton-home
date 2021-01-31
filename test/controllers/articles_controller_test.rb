9e329d3a9b6db901080730f1ec4d8dfea50a6a72
guillermo varela <guillovarela16@gmail.com>
2021-01-30 21:13:55 -0300

    Articles controller in progress

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,23 +1,99 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get articles_index_url
+  setup do
+    login_as users(:admin)
+    @division = divisions(:sheets)
+    @article = articles(:pillow)
+    @article_params = {
+      name: "second_pillow",
+      color: "red",
+      photo: "something",
+      division_id: @division.id
+    }
+  end
+
+  test "wholesaler should get index" do
+    login_as users(:wholesaler)
+
+    get articles_path
+
     assert_response :success
   end
 
-  test "should get show" do
-    get articles_show_url
+  test "admin should get index" do
+    login_as users(:admin)
+
+    get articles_path
+
     assert_response :success
   end
 
-  test "should get edit" do
-    get articles_edit_url
+  test "super_admin should get index" do
+    login_as users(:super_admin)
+
+    get articles_path
+
     assert_response :success
   end
 
-  test "should get new" do
-    get articles_new_url
+  test "buyer should not get index" do
+    login_as users(:buyer)
+
+    get articles_path
+
+    assert_response :redirect
+  end
+
+  test "wholesaler should get show" do
+    login_as users(:wholesaler)
+
+    get article_path(@article)
+
+    assert_response :success
+  end
+
+  test "admin should get show" do
+    login_as users(:admin)
+
+    get article_path(@article)
+
+    assert_response :success
+  end
+
+  test "super_admin should get show" do
+    login_as users(:super_admin)
+
+    get article_path(@article)
+
+    assert_response :success
+  end
+
+  test "buyer should not get show" do
+    login_as users(:buyer)
+
+    get article_path(@article)
+
+    assert_response :redirect
+  end
+
+  test "admin should get new" do
+    get new_article_path
+
+    assert_response :success
+  end
+
+  test "wholesaler should not get new" do
+    login_as users(:wholesaler)
+
+    get new_article_path
+
+    assert_response :redirect
+  end
+
+  test "admin should get edit" do
+    get edit_article_path(@article)
+
     assert_response :success
   end
 
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    login_as users(:admin)
+    @division = divisions(:sheets)
+    @article = Article.new( name: "pillow_second", color: "red", division_id: @division.id)
+    file = URI.open("http://res.cloudinary.com/demo/image/upload/sample.jpg")
+    @article.photos.attach(io: file, filename: 'image.jpg')
+  end
+
+  test "should be valid if all fields are set" do
+    assert @article.valid?
+  end
 end
