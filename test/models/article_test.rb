84b746ea4c74416eaa589ab2bc66fdef8ca2cc44
guillermo varela <guillovarela16@gmail.com>
2021-02-01 11:20:45 -0300

    Finish ariticles testing

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require "open-uri"
 
 class ArticleTest < ActiveSupport::TestCase
   setup do
@@ -11,5 +12,18 @@ class ArticleTest < ActiveSupport::TestCase
 
   test "should be valid if all fields are set" do
     assert @article.valid?
+  end
+
+  test "should be invalid if name is not present" do
+    @article.name = nil
+
+    assert_not @article.valid?
+  end
+
+
+  test "should be invalid if photo is not present" do
+    @article.photos = nil
+
+    assert_not @article.valid?
   end
 end
