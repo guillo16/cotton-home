6ea5c8f82e9f294a8f1f999ca61f3fbefd09bf40
guillermo varela <guillovarela16@gmail.com>
2021-02-01 20:16:58 -0300

    Dimenison controller in progress

--- test/controllers/dimensions_controller_test.rb
+++ test/controllers/dimensions_controller_test.rb
@@ -1,14 +1,42 @@
 require 'test_helper'
 
 class DimensionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get dimensions_new_url
+  setup do
+    login_as users(:admin)
+    @article = articles(:pillow)
+    @dimension = dimensions(:first_dimension)
+    @dimension_params = {
+                          size: "King",
+                          description: "Very nice and expensive",
+                          price: 4535,
+                          clarification: "very long",
+                          article: @article
+                        }
+  end
+
+  test "admin should get show" do
+    get dimensions_path(@dimension)
+
     assert_response :success
   end
 
-  test "should get create" do
-    get dimensions_create_url
-    assert_response :success
+  test "wholesaler should not get show" do
+    login_as users(:wholesaler)
+    get dimensions_path(@dimension)
+
+    assert_response :redirect
   end
 
+  test "buyer should not get show" do
+    login_as users(:buyer)
+    get dimensions_path(@dimension)
+
+    assert_response :redirect
+  end
+
+  test "admin should get new" do
+    get new_dimension_path
+
+    assert_response :success
+  end
 end
