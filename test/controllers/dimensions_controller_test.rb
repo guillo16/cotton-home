638634eecbe03735bcbb5aa0e591da4a102b6a4e
guillermo varela <guillovarela16@gmail.com>
2021-02-02 11:09:20 -0300

    Finish dimensions testing

--- test/controllers/dimensions_controller_test.rb
+++ test/controllers/dimensions_controller_test.rb
@@ -15,28 +15,91 @@ class DimensionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "admin should get show" do
-    get dimensions_path(@dimension)
+    get dimension_path(@dimension)
 
     assert_response :success
   end
 
   test "wholesaler should not get show" do
     login_as users(:wholesaler)
-    get dimensions_path(@dimension)
+    get dimension_path(@dimension)
 
     assert_response :redirect
   end
 
   test "buyer should not get show" do
     login_as users(:buyer)
-    get dimensions_path(@dimension)
+    get dimension_path(@dimension)
 
     assert_response :redirect
   end
 
   test "admin should get new" do
-    get new_dimension_path
+    get new_article_dimension_url(@article.id)
 
     assert_response :success
   end
+
+
+  test "buyer should not get new" do
+    login_as users(:buyer)
+
+    get new_article_dimension_url(@article.id)
+
+    assert_response :redirect
+  end
+
+  test "wholesaler should not get new" do
+    login_as users(:wholesaler)
+
+    get new_article_dimension_url(@article.id)
+
+    assert_response :redirect
+  end
+
+  test "create should be success" do
+    assert_difference "Dimension.count", 1 do
+      post article_dimensions_url(@article.id), params: { dimension: @dimension_params }
+    end
+
+    assert_redirected_to article_path(@article)
+  end
+
+  test "admin should get edit" do
+    get edit_dimension_path(@dimension)
+
+    assert_response :success
+  end
+
+  test "buyer should not get edit" do
+    login_as users(:buyer)
+    get edit_dimension_path(@dimension)
+
+    assert_response :redirect
+  end
+
+  test "wholesaler should not get edit" do
+    login_as users(:wholesaler)
+    get edit_dimension_path(@dimension)
+
+    assert_response :redirect
+  end
+
+  test "update should be success" do
+    patch dimension_path(@dimension), params: { dimension: { size: "small" } }
+
+    assert_redirected_to dimension_path(@dimension)
+    @dimension.reload
+
+    assert_equal "small", @dimension.size
+  end
+
+  test "destroy should be success" do
+    assert_difference("Dimension.count", -1) do
+      delete dimension_path(@dimension)
+    end
+
+    assert_redirected_to article_path(@article)
+  end
+
 end
