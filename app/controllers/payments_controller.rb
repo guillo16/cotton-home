3474789a2dba5f664a5c79a6075f039dfa1f15ac
guillermo varela <guillovarela16@gmail.com>
2021-02-10 16:58:27 -0300

    Almost finished

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,17 +1,18 @@
 
 class PaymentsController < ApplicationController
   before_action :set_order, only: %i[new create]
+
   def new
     @order = current_user.orders.where(state: 'Pendientes').find(params[:order_id])
     require 'mercadopago.rb'
-    mp = MercadoPago.new('TEST-4172019108926178-021314-b2f2034cf02d63f6135dc75afbea4744-403897306')
+    mp = MercadoPago.new(ENV['ACCESS_TOKEN'])
 
     # Crea un objeto de preferencia
 
     preference_data = {
       "items": [
         {
-          "title": "Mi producto",
+          "title": "Mi Orden",
           "unit_price": @order.amount.to_i,
           "quantity": 1,
           "currency_id": "ARS"
@@ -23,13 +24,16 @@ class PaymentsController < ApplicationController
   end
 
   def create
-  @cart = Cart.find(session[:cart_id])
-  session[:cart_id] = nil
-  @order.update(state: 'Encargado')
-  OrderMailer.with(order: @order).new_order.deliver_later
-  OrderMailer.with(order: @order).new_payment.deliver_later
-  redirect_to order_path(@order)
-
+    if params[:payment_status] == "approved"
+      @cart = Cart.find(session[:cart_id])
+      session[:cart_id] = nil
+      @order.update(state: 'Encargado')
+      OrderMailer.with(order: @order).new_order.deliver_later
+      OrderMailer.with(order: @order).new_payment.deliver_later
+      redirect_to order_path(@order)
+    else
+      redirect_to new_order_payment_path(@order)
+    end
     # OrderMailer.with(order: @order).new_order.deliver_later
     # OrderMailer.with(order: @order).new_payment.deliver_later
   end
