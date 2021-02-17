97afcf8415fd146cc63f83a3140edc4207ef822b
guillermo varela <guillovarela16@gmail.com>
2021-02-17 10:27:19 -0300

    Add fields to shippings

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,11 +1,24 @@
 class Shipping < ApplicationRecord
+  before_save :format_name
+
   belongs_to :order
   belongs_to :user
 
   validates :address, presence: true
   validates :amount, presence: true
   validates :city, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :first_name, presence: true, length: { in: 2..20, allow_blank: true }
+  validates :last_name, presence: true, length: { in: 2..20, allow_blank: true }
   validates :state, presence: true
+
+  def full_name
+    "#{first_name.capitalize} #{last_name.capitalize}".strip
+  end
+
+  private
+
+  def format_name
+    self.first_name = first_name.split(" ").map(&:capitalize).join(" ")
+    self.last_name = last_name.split(" ").map(&:capitalize).join(" ")
+  end
 end
