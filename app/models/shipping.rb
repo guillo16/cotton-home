a6d8a78f0b1000d4ae82cc8a03f85bd2e38f584d
guillermo varela <guillovarela16@gmail.com>
2021-02-17 10:36:50 -0300

    Finish shipping validations

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -6,13 +6,25 @@ class Shipping < ApplicationRecord
 
   validates :address, presence: true
   validates :amount, presence: true
+  validates :area_code, length: { in: 2..5 }, numericality: true, allow_blank: true
+  validates :area_code, presence: true
+  validates :building, lenght: { in: 1..8 }
   validates :city, presence: true
+  validates :dni, presence: true, length: { in: 7..8, allow_blank: true }
+  validates :floor, lenght: { in: 1..2 }, numericality: true
   validates :first_name, presence: true, length: { in: 2..20, allow_blank: true }
   validates :last_name, presence: true, length: { in: 2..20, allow_blank: true }
+  validates :number, presence: true, numericality: true, allow_blank: true
+  validates :phone, length: { in: 5..9 }, numericality: true, allow_blank: true
+  validates :phone, presence: true
   validates :state, presence: true
 
   def full_name
     "#{first_name.capitalize} #{last_name.capitalize}".strip
+  end
+
+  def full_number
+    "#{area_code}-#{phone}"
   end
 
   private
@@ -20,5 +32,13 @@ class Shipping < ApplicationRecord
   def format_name
     self.first_name = first_name.split(" ").map(&:capitalize).join(" ")
     self.last_name = last_name.split(" ").map(&:capitalize).join(" ")
+  end
+
+  def sanitize_area_code
+    self.area_code = area_code&.delete("^0-9")
+  end
+
+  def sanitize_phone
+    self.phone = phone&.delete("^0-9")
   end
 end
