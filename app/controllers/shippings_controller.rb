63615c93ad83603dddc74ea1e2dea08510e31cbf
guillermo varela <guillovarela16@gmail.com>
2021-03-17 19:54:36 -0300

    Shipping for zones finished

--- app/controllers/shippings_controller.rb
+++ app/controllers/shippings_controller.rb
@@ -7,10 +7,12 @@ class ShippingsController < ApplicationController
     @shipping.order = @order
     @shipping.user = current_user
     if @shipping.save
-      if @shipping.shipping_method == 'caba'
-        @shipping.amount = 400
-      elsif @shipping.shipping_method == 'address'
+      if @shipping.shipping_method == 'one'
+        @shipping.amount = 500
+      elsif @shipping.shipping_method == 'two'
         @shipping.amount = 600
+      elsif @shipping.shipping_method == 'three'
+        @shipping.amount = 700
       else
         @shipping.amount = 0
       end
@@ -25,15 +27,16 @@ class ShippingsController < ApplicationController
 
   def update
     if @shipping.update(shipping_params)
-      if @shipping.shipping_method == 'caba'
-        @shipping.amount = 400
-      elsif @shipping.shipping_method == 'address'
-        shipping_total = 600
+      if @shipping.shipping_method == 'one'
+        @shipping.amount = 500
+      elsif @shipping.shipping_method == 'two'
+        @shipping.amount = 600
+      elsif @shipping.shipping_method == 'three'
+        @shipping.amount = 700
       else
-        shipping_total = 0
+        @shipping.amount = 0
       end
-      shipping = shipping_total
-      @shipping.order.update(total: @shipping.order.amount.to_i + shipping)
+      @shipping.order.update(total: @shipping.order.amount.to_i + @shipping.amount)
       redirect_to new_order_payment_path(@shipping.order.id)
     else
       render :edit
