45af150c846e4ad9d6e79024be1e674f7e27c377
guillermo varela <guillovarela16@gmail.com>
2021-04-12 13:13:26 -0300

    Create shipping yml

--- test/controllers/shippings_controller_test.rb
+++ test/controllers/shippings_controller_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class ShippingsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    login_as users(:admin)
+    @order = orders(:first_order)
+    @shipping = shippings(:first_shipping)
+    @shipping_params = {
+      address: "Ruben dario y Algo",
+      area_code: 123,
+      building: "6a",
+      city: "Yerba Buena",
+      dni: 31323232,
+      floor: 5,
+      first_name: "Joe",
+      last_name: "Doe",
+      number: 152,
+      phone: 5888888,
+      postal_code: 3245,
+      shipping_method: "one",
+      state: "Tucuman",
+    }
+  end
+
+  test "create should be a success" do
+    assert_difference "Shipping.count", 1 do
+      post order_shippings_url(@order), params: { shipping: @shipping_params }
+    end
+  end
 end
