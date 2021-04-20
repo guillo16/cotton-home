eedf6a6905c8c6dcda6ca942d1d3cfeae1ba1c17
guillermo varela <guillovarela16@gmail.com>
2021-04-20 16:44:10 -0300

    Finish locales

---
+++ app/models/contact.rb
@@ -0,0 +1,18 @@
+class Contact < MailForm::Base
+  attribute :name, validate: true
+  attribute :email, validate: /\A[^@\s]+@[^@\s]+\z/i
+  attribute :file, attachment: true
+
+  attribute :message, validate: true
+  attribute :nickname, captcha: true
+
+  # Declare the e-mail headers. It accepts anything the mail method
+  # in ActionMailer accepts.
+  def headers
+    {
+      subject: "Contacto Cotton Home",
+      to: "carlipassaglia@hotmail.com",
+      from: %("#{name}" <#{email}>)
+    }
+  end
+end
